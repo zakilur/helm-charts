4b72d440e8b478cb8a94b871c8186db2702de856
Alex Kreidler <alex.kreidler@deciphernow.com>
2019-08-05 15:49:53 -0400

    rename and move oldtown to gm-control-api, update various docker images

--- gm-control-api/config/bootstrap.sh
+++ gm-control-api/config/bootstrap.sh
@@ -9,7 +9,7 @@ cd $MESH_CONFIG_DIR
 echo "Config dir services:"
 ls
 
-# This script expects oldtown to be up and available to serve requests
+# This script expects gm-control-api to be up and available to serve requests
 # Currently, this is handled in a fairly good idiomatic way using Readiness Probes and `k8s-waiter`
 
 echo "Starting mesh configuration ..."
@@ -18,7 +18,7 @@ for d in */; do
     echo "Found service: $d"
     cd $d
 
-    # The ordering of creating oldtown resources is extremely important and precise.
+    # The ordering of creating gm-control-api resources is extremely important and precise.
     # All objects referenced by keys must be created before being referenced or will result in an error.
     # So we add a delay of 0.1 seconds between each request to hopefully streamline this
     # A better option is probably to hardcode the order of items
