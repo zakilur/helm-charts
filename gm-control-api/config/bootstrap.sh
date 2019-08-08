f482afd3ff8de417a90ccff3fb06ae5d683e5abf
Alex Kreidler <alex.kreidler@deciphernow.com>
2019-08-08 14:52:16 -0400

    finished templating

--- gm-control-api/config/bootstrap.sh
+++ gm-control-api/config/bootstrap.sh
@@ -6,7 +6,7 @@ echo "Configuring mesh from config directory: $MESH_CONFIG_DIR"
 
 cd $MESH_CONFIG_DIR
 
-echo "Config dir services:"
+echo "Config dir contains:"
 ls
 
 # This script expects gm-control-api to be up and available to serve requests
@@ -14,23 +14,10 @@ ls
 
 echo "Starting mesh configuration ..."
 
-echo "Creating basic objects, using path: $BASIC_OBJECT_PATH"
-
-cd $BASIC_OBJECT_PATH
-
-for objtype in */; do
-    echo "Found basic object type: $objtype"
-    echo "Creating basic objects in directory"
-    for obj in *.json; do
-        echo "Found $obj"
-        greymatter create $objtype < $obj
-    done
-done
-
 echo "Creating service configuration objects..."
 
-greymatter create domain <$DOMAIN_FILE_PATH
-
+cd services
+# Each service should be able to be created all by itself. This means it needs to contain a domain
 for d in */; do
     echo "Found service: $d"
     cd $d
@@ -40,18 +27,39 @@ for d in */; do
     # So we add a delay of 0.1 seconds between each request to hopefully streamline this
     # A better option is probably to hardcode the order of items
 
-    names="cluster listener proxy shared_rules"
+    names="domain cluster listener proxy shared_rules route"
     for name in $names; do
         echo "Creating mesh object: $name."
         greymatter create $name <$name.json
-        # sleep 0.1
+    done
+
+done
+cd $MESH_CONFIG_DIR
+
+# The edge service is created last as it links to the clusters of every other service.
+
+# The edge domain must be created before it can be referenced
+
+cd domain
+
+greymatter create domain < domain.json
+
+cd $MESH_CONFIG_DIR
+cd edge
+
+# All the following services reference the `edge` domain key
+for d in */; do
+    echo "Found service: $d"
+    cd $d
+
+    names="cluster shared_rules"
+    for name in $names; do
+        echo "Creating mesh object: $name."
+        greymatter create $name <$name.json
     done
 
     for file in route-*.json; do
         echo "Creating mesh object: $name."
         greymatter create route <$file
-        # sleep 0.1
     done
-
-    cd $MESH_CONFIG_DIR
 done
