30ad8de5f65d18b6913f28ce6330956de85eb66d
zoemccormick <zmm7@cornell.edu>
2020-03-24 14:36:49 +0000

    catalog init on upgrade

--- sense/catalog/config/bootstrap.sh
+++ sense/catalog/config/bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-MESH_CONFIG_DIR="/etc/config/mesh/"
+MESH_CONFIG_DIR="/etc/config/mesh"
 CURL_COMMAND='curl -s -o /dev/null -w "%{http_code}"'
 HTTP="http"
 
@@ -32,7 +32,7 @@ echo "Starting catalog configuration ..."
 
 delay=0.01
 
-sendCommand() {
+send_or_update() {
     if [ "$DEBUG" == "true" ]; then
         echo "Contents of $2..."
         echo
@@ -40,40 +40,74 @@ sendCommand() {
         echo
     fi
 
-    echo "Uploading $file..."
-    http_response=$(${CURL_COMMAND} -X POST -d @$2 $HTTP://$CATALOG_API_HOST/$1)
-    if [ "$DEBUG" == "true" ]; then
-        echo $http_response
-    fi 
+    echo "Checking for $1 $file..."
+    http_response=$(${CURL_COMMAND} -X GET -d @$2 $HTTP://$CATALOG_API_HOST/$1/$3)
     echo $http_response
+
     http_response="${http_response%\"}"
     http_response="${http_response#\"}"
+    echo $http_response
 
-    if [ $http_response != "200" ]; then
-        echo "There was an error uploading a file to catalog.  Exiting"
-        exit 1
-    fi
+    if [ $http_response == "404" ]; then
+        echo "Uploading $1 $3 $file..."
+        http_response=$(${CURL_COMMAND} -X POST -d @$2 $HTTP://$CATALOG_API_HOST/$1)
+        if [ "$DEBUG" == "true" ]; then
+            echo $http_response
+        fi 
+
+        http_response="${http_response%\"}"
+        http_response="${http_response#\"}"
+
+        if [ $http_response != "200" ]; then
+            echo "There was an error uploading file $file to catalog.  Exiting"
+            exit 1
+        fi
+
+    elif [ $http_response == "200" ] && [ $1 == "clusters" ]; then
+        echo "$1 $3 found, updating"
+        http_response=$(${CURL_COMMAND} -X PUT -d @$2 $HTTP://$CATALOG_API_HOST/$1/$3)
+        if [ "$DEBUG" == "true" ]; then
+            echo $http_response
+        fi 
+
+        http_response="${http_response%\"}"
+        http_response="${http_response#\"}"
+
+        if [ $http_response != "200" ]; then
+            echo "There was an error uploading file $file to catalog.  Exiting"
+            exit 1
+        fi
     
+    elif [ $http_response == "200" ] && [ $1 == "zones" ]; then
+        echo "$1 $3 found, zone already exists. Continuing..."
+
+    else
+        echo "There was a problem getting the file $file"
+        exit 1
+
+    fi
+
 }
 
 cd $MESH_CONFIG_DIR/zones
 echo "Loading the zones to catalog ..."
 for file in $(ls); do
-    sendCommand zones $file
+    send_or_update zones $file ${file%.json}
 done
 
 cd $MESH_CONFIG_DIR/services
 for d in */; do
     echo
-    echo "Found service: $d"
+    echo "Found service: ${d%/}"
     cd $d
 
     for file in $(ls); do
         echo "Creating catalog item $file"
-        sendCommand clusters $file
+        send_or_update clusters $file ${d%/}
     done
 
     cd $MESH_CONFIG_DIR/services
 done
 
 echo "Catalog configuration complete"
+
