0b1aedaba56fd07b16052be447b47f0ed1931a4e
kaitmore <kaitmore@gmail.com>
2020-09-09 17:33:17 -0400

    performance tests

--- ci/scripts/k3d.sh
+++ ci/scripts/k3d.sh
@@ -3,8 +3,7 @@
 # install k3d 3.0.0
 NAME=greymatter
 
-if ! command -v k3d &> /dev/null
-then
+if ! command -v k3d &>/dev/null; then
     echo "*** k3d must be installed to start a kubernetes cluster with k3d. Install here: https://k3d.io/#installation ***"
     exit
 fi
@@ -14,14 +13,12 @@ len=${#output}
 version=${output:13:1}
 default="3"
 
-if ((version < default)); 
-    then
-        echo '***Please update k3d to v3.0.0 or greater***'
-        exit
+if ((version < default)); then
+    echo '***Please update k3d to v3.0.0 or greater***'
+    exit
 fi
- 
 
-k3d cluster create $NAME -a 4 -p 30000:10808@loadbalancer && sleep 10
+k3d cluster create $NAME -a 8 -p 30000:10808@loadbalancer && sleep 10
 
 export KUBECONFIG=$(k3d kubeconfig write greymatter)
 
